61e4e3f707860a02d7b176c75d4587ef39bdd26f
Yuri Marlily Celis Torres <79286202+marlilycelis@users.noreply.github.com>
2021-02-18 17:20:50 -0500

    Cuentas de Banco
    Ejercicios para gestionar cuentas donde se ingresa, retira, crea y elimina cuentas del banco.

---
+++ ClasesYO_Cajero/ClasesYO_Cajero/Cuenta.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesYO_Cajero
+{
+    public class Cuenta
+    {
+        /* Se definen 2 campos (atributos) para la clase */
+        protected string titular;
+        protected float cantidad;
+
+        public Cuenta(string titular, float cantidad = 0)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string getTitular()
+        {
+            return this.titular;
+        }
+
+        public float getSaldo()
+        {
+            return this.cantidad;
+        }
+
+        /* Método que permite agregar saldo a una cuenta. 
+         * Recibe un numeros decimal de tipo flotante. 
+         * Retorna un String */
+        public string Ingresar(float cantidadRecibida)
+        {
+            if(cantidadRecibida < 0)
+            {
+                return "Que pena, la cantidad es negativa, no puedo hacer nada";
+            }
+            else
+            {
+                this.cantidad = this.cantidad + cantidadRecibida;
+                return "La cantidad fue agregada, el nuevo total es " + this.cantidad;
+            }
+        }
+
+        /* Función para disminuir el saldo de una cuenta.
+         * IN: float cantidadARetirar
+         * OUT: String
+         * */
+        public string Retirar(float cantidadARetirar)
+        {
+            float subtotal = this.cantidad - cantidadARetirar;
+            if(subtotal < 0)
+            {
+                string mensaje = "No tienes todo ese dinero, solo te daré " + this.cantidad;
+                this.cantidad = 0;
+                return mensaje;
+            }
+            else
+            {
+                this.cantidad = subtotal;
+                return "Usted ha retirado " + cantidadARetirar + ". El saldo restante es " + subtotal;
+            }
+        }
+    }
+}
---
+++ ClasesYO_Cajero/ClasesYO_Cajero/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesYO_Cajero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Programa que permite agregar cuentas y hacer transacciones con ella
+             Tarea
+            */
+
+            List<Cuenta> cuentas = new List<Cuenta>();
+            Cuenta comodin;
+            float aRetirar, aConsignar;
+            string respuesta, cedula, continuar, rmenu;
+
+            Console.WriteLine("Bienvenido a la aplicación");
+
+            do
+            {
+                Console.WriteLine("Selecciones la acción a realizar");
+                Console.WriteLine("1 para crear cuenta");
+                Console.WriteLine("2 para consignar");
+                Console.WriteLine("3 para retirar");
+                Console.WriteLine("4 para consultar cuenta");
+                Console.WriteLine("5 para listar las cuentas");
+                Console.WriteLine("6 para eliminar cuenta");
+                Console.WriteLine("7 para transferir");
+                rmenu = Console.ReadLine();
+
+                switch (rmenu)
+                {
+                    case "1":
+                        Console.WriteLine("Por favor ingrese la cédula del titular de la cuenta");
+                        cedula = Console.ReadLine();
+                        comodin = CrearCuenta(cedula);
+                        cuentas = AgregarALista(comodin, cuentas);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Por favor ingrese la cédula del titular de la cuenta");
+                        cedula = Console.ReadLine();
+                        Console.WriteLine("Por favor ingrese la el valor a consignar");
+                        aConsignar = float.Parse(Console.ReadLine());
+                        ////////
+                        break;
+                    case "3":
+                        Console.WriteLine("Por favor ingrese la cédula del titular de la cuenta");
+                        cedula = Console.ReadLine();
+                        Console.WriteLine("Por favor ingrese la el valor a retirar");
+                        aRetirar = float.Parse(Console.ReadLine());
+                        ////////
+                        break;
+                    case "4":
+                        Console.WriteLine("Por favor ingrese la cédula del titular de la cuenta");
+                        cedula = Console.ReadLine();
+                        comodin = ConsultarCuenta(cedula, cuentas);
+                        if(comodin != null)
+                        {
+                            ImprimirCuenta(comodin);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Que pena, esa cuenta no la encontré");
+                        }
+                        break;
+                    case "5":
+                        ImprimirCuentas(cuentas);
+                        break;
+                }
+
+
+                Console.WriteLine("¿Desea continuar haciendo algun proceso? (si o no)");
+                continuar = Console.ReadLine();
+            } while (continuar == "si");
+
+            Console.ReadKey();
+        }
+
+        static void ImprimirCuentas(List<Cuenta> cuentas)
+        {
+            Console.WriteLine("Listando cuentas");
+            Console.WriteLine("-------------------------\n");
+            foreach (Cuenta iterador in cuentas)
+            {
+                Console.WriteLine("Titular: " + iterador.getTitular());
+                Console.WriteLine("Saldo: " + iterador.getSaldo());
+                Console.WriteLine("-------------------------\n");
+            }
+        }
+
+        static void ImprimirCuenta(Cuenta elemento)
+        {
+            Console.WriteLine("Información cuentas");
+            Console.WriteLine("Titular: " + elemento.getTitular());
+            Console.WriteLine("Saldo: " + elemento.getSaldo());
+            Console.WriteLine("-------------------------\n");
+        }
+
+        static Cuenta CrearCuenta(string titular)
+        {
+            Cuenta cuentica = new Cuenta(titular);
+            return cuentica;
+        }
+
+        static List<Cuenta> AgregarALista(Cuenta elemento, List<Cuenta> lista)
+        {
+            string cedula = elemento.getTitular();
+            Cuenta validadora = ConsultarCuenta(cedula, lista);
+            if(validadora == null)
+            {
+                lista.Add(elemento);
+            }
+            return lista;
+        }
+
+        static Cuenta ConsultarCuenta(string cedula, List<Cuenta> cuentas)
+        {
+            foreach (Cuenta c in cuentas)
+            {
+                if (c.getTitular().Equals(cedula))
+                {
+                    return c;
+                }
+            }
+            return null;
+        }
+    }
+}

57e40dd854f57d535e7e4b2c09d86aba1069f30f
Yuri Marlily Celis Torres <79286202+marlilycelis@users.noreply.github.com>
2021-02-19 17:45:05 -0500

    Ejemplo de Interfaz
    Ejemplo de Interfaz

---
+++ InterfazVehiculos/Entidades/TanqueGuerra.cs
@@ -0,0 +1,48 @@
+﻿using InterfazVehiculos.Interfaz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfazVehiculos.Entidades
+{
+    class TanqueGuerra : IVehiculo,IArmaFuego
+    {
+        public int CantidadRuedas { get; protected set; }
+        public int PotenciaFuego { get; set; }
+        public float VelocidadMaximaKm_H { get; set; }
+        public float CapacidadMaximaCargaToneladas { get; set; }
+        public bool ManejoControlRemoto { get; set; }
+        public void acelerar()
+        {
+            Console.WriteLine("Estoy acelarando");
+        }
+
+        public void apagar()
+        {
+            Console.WriteLine("Tanque Apagado");
+        }
+
+        public void Cargar()
+        {
+            Console.WriteLine("Arma Cargada lista para disparar");
+        }
+
+        public void Disparar()
+        {
+            Console.WriteLine("Disparar PUM - PUM - PUM");
+        }
+
+        public void encender()
+        {
+            Console.WriteLine("Estoy encendida");
+        }
+
+        public void frenar()
+        {
+            Console.WriteLine("Estoy desacelarando");
+        }
+    }
+}
+       
---
+++ InterfazVehiculos/Interfaz/IVehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfazVehiculos.Interfaz
+{
+    interface IVehiculo
+    {
+        void encender();
+        void apagar();
+        void acelerar();
+        void frenar();
+    }
+}
